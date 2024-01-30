9d5b6235f685656ec3b5a21b59d8ae32fe181158
Kunal Malav <kunalmalav@icloud.com>
2024-01-30 22:32:30 +0530

    do more

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
-import Header from "./Header.jsx"
+import Header from "./Header.jsx";
+import Footer from "./Footer.jsx";
+import Note from "./Note.jsx"
 
 function App()
 {
-    return <div> <Header /></div>
+    return (<div> 
+    <Header />
+    <Note />
+    <Footer />
+    </div>
+    );
 }
 
 export default App;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,9 +2,10 @@ import React from 'react';
 
 function Header()
 {
-  return <header>
+  return (<header>
     <h1>Keeper</h1>
     </header>
+  );
 }
 
 export default Header;
