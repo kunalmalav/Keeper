aed93d7b4bbbb7cf5c0a9b854b17fa5f1c73dc01
Kunal Malav <kunalmalav@icloud.com>
2024-02-01 21:09:58 +0530

    add functional components

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,27 @@
-import React from "react";
+import React, { useState } from 'react';
 import Header from "./Header.js";
 import Footer from "./Footer.js";
 import Note from "./Note.js";
-import notes from "../notes.js";
+import createArea from "./createArea.jsx";
+
 
 function App() {
+    
+    const [ notes, setNotes ] = useState([])
+
+    function addNote(newNote){
+        setNotes(prevNotes => {
+            return [...prevNotes, newNote];
+        });
+    }
+
   return (
     <div>
       <Header />
-      {notes.map(noteItem => (
-        <Note
-          key={noteItem.key}
-          title={noteItem.title}
-          content={noteItem.content}
-        />
-      ))}
+      <createArea 
+      onAdd = {addNote}
+      />
+      <Note key={1} title="Note title" content="Note content" />
       <Footer />
     </div>
   );
